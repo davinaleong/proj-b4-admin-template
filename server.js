8cd3722d833ec7ee820608f40a5e9d6095824261
Davina Leong <davina.leong.sy@gmail.com>
2018-09-20 06:50:52 +0800

    Added GitHub ribbon

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const path = require('path');
 const app = express();
 
 const config = {
-    dir: 'dist',
+    root: './',
+    dir: './dist',
     port: 3000
 };
 
 app.use(express.static(config.dir));
 app.use(`/${config.dir}`, express.static(path.join(__dirname, config.dir)));
+app.use(`/${config.root}`, express.static(path.join(__dirname, config.root)));
 
 app.listen(config.port, () => {
     console.log(`Server started on port ${config.port}.`);
